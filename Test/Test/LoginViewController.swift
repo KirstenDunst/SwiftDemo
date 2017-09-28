ea13395e727a13b6d64c9fc4fe7baaf963903b9a
KirstenDunst <cao_shixin@yahoo.com>
2017-09-28 17:26:32 +0800

    Alamofire添加请求处理get，post。以及一些数据请求响应处理的方法使用

--- Test/Test/LoginViewController.swift
+++ Test/Test/LoginViewController.swift
@@ -50,6 +50,7 @@
 import UIKit
 import SnapKit
 import Kingfisher
+import Alamofire
 
 class LoginViewController: UIViewController {
 
@@ -60,11 +61,6 @@ class LoginViewController: UIViewController {
 //        createView(a: 12)
         createViewOne();
         // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
     func createView(a:Int) -> Int {
@@ -79,6 +75,8 @@ class LoginViewController: UIViewController {
         imageView.layer.masksToBounds = true;
         self.view.addSubview(imageView)
 //        imageView.image = UIImage.init(named: "123")!
+        
+        
         
         
         
@@ -124,14 +122,138 @@ class LoginViewController: UIViewController {
         
 //        使用系统自带的请求封装的方法进行网络请求处理
         CSHTTPTool.share.getWithPath(path: "http://appapi.zongs365.com/api/home/machine/9001000015/goodsdetial", paras: nil, success: { (data) in
-            print("成功","\n", data)
+//            print("成功","\n", data)
         }) { (error) in
-            print("失败", error)
+//            print("失败", error)
+        }
+        
+        
+        
+        
+        
+        
+        
+        
+        
+//        使用Alamofire的第三方调用数据请求获得返回数据
+        GET_Request();
+        
+    }
+    
+    //    get
+    func GET_Request() {
+        let parameters : [String : Any] = ["foo": "bar"]
+        
+        //1,responseJSON
+        Alamofire.request("http://appapi.zongs365.com/api/home/machine/9001000015/goodsdetial", method: .get, parameters: parameters).responseJSON { (returnResult) in
+            print("GET_Request --> GET 请求 --> returnResult = \(returnResult)")
+            //            请求成功之后返回的数据SUCCESS: {}
+//            使用下面的方法获得下面的内部的返回json内容，除去了SUCCESS: {}的外部的内容。获得成功之后的value值
+            if let json = returnResult.result.value {
+                print("firstMethod --> responseJSON --> \(json)")
+                /*  返回请求地址、数据、和状态结果等信息
+                 print("firstMethod --> responseJSON() --> \(returnResult.request!)")
+                 print("firstMethod --> responseJSON() --> \(returnResult.data!)")
+                 print("firstMethod --> responseJSON() --> \(returnResult.result)")
+                 */
+            }
+        }
+    }
+    // post
+    func POST_Request(){
+        let  host_url = String()
+        let  top = String()
+        let  appkey = String()
+        //request(host_url, method:.post, parameters : parameters)
+        let urlstring = "\(host_url)type=\(top)&key=\(appkey)"
+        Alamofire.request(urlstring, method:.post).responseJSON { (returnResult) in
+            print("POST_Request --> post 请求 --> returnResult = \(returnResult)")
+            //            switch returnResult.result.isSuccess {
+            //            case true:
+            //                print("数据获取成功!")
+            //            case false:
+            //                print(returnResult.result.error ?? Error.self)
+            //            }
+            
+        }
+    }
+        
+    //下载 上传
+    func downloadAnduploadMethod() {
+        let  host_img_url = String()
+        //下载文件
+        Alamofire.download(host_img_url).responseJSON { (returnResult) in
+            if let data = returnResult.result.value {
+                let image = UIImage(data : data as! Data)
+                print("\(String(describing: image))")
+            }else {
+                print("download is fail")
+            }
+        }
+        //还可以看下载进度
+        Alamofire.download(host_img_url).downloadProgress { (progress) in
+            print("download progress = \(progress.fractionCompleted)")
+            }.responseJSON { (returnResult) in
+                if let data = returnResult.result.value {
+                    let image = UIImage(data : data as! Data)
+                    print("\(String(describing: image))")
+                }else {
+                    print("download is fail")
+                }
         }
     }
     
+    //    响应处理（Response Handling)  除了上面样例使用的responseJSON（处理json类型的返回结果）外，Alamofire还提供了许多其他类型的响应处理方法：
+    /*
+     response()
+     responseData()
+     responseString(encoding: NSStringEncoding)
+     responseJSON(options:NSJSONReadingOptions)
+     responsePropertyList(options: NSPropertyListReadOptions)
+ */
+    //2,response()
+    //        Alamofire.request(main_url, method:.get, parameters: parameters).response { (response) in
+    //            print("response = \(response.response)")
+    //            print("data = \(response.data)")
+    //            print("error = \(response.error)")
+    //            if let data = response.data , let utf8string = String(data: data , encoding:.utf8) {
+    //                print("utf8string = \(utf8string)")
+    //            }
+    //        }
+    
+    //3,responseData()
+    //        Alamofire.request(main_url, method: .get, parameters: parameters).responseData { (responseData) in
+    //            debugPrint("responseData : \(responseData)")
+    //            if let data = responseData.data, let utf8string = String(data: data, encoding: .utf8) {
+    //                print("utf8string = \(utf8string)")
+    //            }
+    //        }
+    
+    //        //4,responseString
+    //        Alamofire.request(main_url, method: .get, parameters: parameters).responseString { (responseString) in
+    //            debugPrint("responseString() --> Sucess = \(responseString.result.isSuccess)")
+    //             debugPrint("responseString : \(responseString)")
+    //            if let data = responseString.data , let utf8string = String(data: data, encoding: .utf8) {
+    //               print("utf8string = \(utf8string)")
+    //            }
+    //        }
+    
+    // 5. responsePropertyList()  下面解释
+    
+    //6.在response方法中还有一个方法  参数：queue：请求队列 --> 就是默认在主线程中执行~但是我们可以自定义调度队列。
+    //        let customQueue = DispatchQueue.global(qos: .utility)
+    //        Alamofire.request(main_url, method: .get, parameters: parameters).responseJSON(queue: customQueue) { (returnResult) in
+    //            print("请求队列 --> \(returnResult)")
+    //        }
     
     
+    
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
     
     /*
     // MARK: - Navigation
